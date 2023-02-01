0596002e0826a64869b0432a3882b7eff5c665d7
UnluckySparow <mouad.ait05@gmail.com>
2023-02-01 01:40:41 +0100

    header css done

--- src/navbar/Navbar.js
+++ src/navbar/Navbar.js
@@ -3,19 +3,23 @@ import '../styles/Navbar.css'
 import mouadimg from "../assets/images/mouadimg.jpeg"
 import { Link } from 'react-router-dom'
 export default function Navbar() {
-  const adminapp = 'mouad'
+  const adminapp = 'mouad';
+  const curentyear = new Date().getFullYear();
   return (
     <div className='section_header'>
       <nav className='containernav'>
         <div className='img_perso'>
             <img src={mouadimg}  alt=""/>
-            <h3>{adminapp}</h3>
         </div>
         <div className='menu'>
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
-            <Link to="/contact">Contact</Link>
-          
+          <ul className="listmenu">
+            <li className='active'><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+          </ul>
+        </div>
+        <div className='copyright_div'>
+          © {curentyear} {adminapp}
         </div>
       </nav>
     </div>
